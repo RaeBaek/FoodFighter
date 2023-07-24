ded08b9ec32fc4d5f5aede3d696592ac04fb28ad
RaeBaek <bmp117@naver.com>
2023-07-24 10:09:41 +0900

    CommitViewController 추가

---
+++ FoodFighter/CommitViewController.swift
@@ -0,0 +1,17 @@
+//
+//  CommitViewController.swift
+//  FoodFighter
+//
+//  Created by 백래훈 on 2023/07/24.
+//
+
+import UIKit
+
+class CommitViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
